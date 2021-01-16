2f22cdbb51cd1c51f9cc93e1047df70b4b31c6ee
Kai <65467226+KyleBerdy@users.noreply.github.com>
2021-01-16 13:20:07 -0500

    Add files via upload
    Basic GUI, let's figure out the layout now

---
+++ src/Application.java
@@ -0,0 +1,34 @@
+import java.awt.EventQueue;
+import javax.swing.*;
+
+/*
+* Version - 0.0.0
+*
+*
+*
+* Sources:
+* -- Golden Bandicoot Image: http://animalia.bio/golden-bandicoot
+* */
+
+public class Application extends JFrame {
+    public Application() {
+        initUI();
+    }
+
+    private void initUI() {
+        add(new Board());
+
+        setSize(250, 200);
+
+        setTitle("Application");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+    }
+
+    public static void main(String[] args) {
+        EventQueue.invokeLater(() -> {
+            Application ex = new Application();
+            ex.setVisible(true);
+        });
+    }
+}
