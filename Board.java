a374ed48c7ee52e5c1b69c5b3b577d34fc792ff0
lisareichelson <73553944+lisareichelson@users.noreply.github.com>
2021-01-16 20:22:46 -0500

    Add files via upload
    Collision files edited, score variable collected.

---
+++ Board.java
@@ -0,0 +1,83 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class Board extends JPanel {
+    private Timer timer;
+    public Bandy bandy = new Bandy();
+    private Image player;
+    public Exit button = new Exit();
+    private int timeDelay = 50;
+    private int level = 1;
+    private int score = 0;
+    public Bug[] bugs = {new Bug(), new Bug(), new Bug(), new Bug(), new Bug(),
+                         new Bug(), new Bug(), new Bug(), new Bug(), new Bug(),
+                         new Bug(), new Bug(), new Bug(), new Bug(), new Bug(),
+                         new Bug(), new Bug(), new Bug(), new Bug(), new Bug(),
+                         new Bug(), new Bug(), new Bug(), new Bug(), new Bug(),
+                         new Bug(), new Bug()};
+
+
+    public Board() {
+        initBoard();
+    }
+
+    private void initBoard() {
+        addKeyListener(new TAdapter()); // Takes in input from keyboard
+        setBackground(Color.BLACK); // Default background color
+        setFocusable(true); // Allows for focus on mouse and keyboard on object
+
+        initGame();
+    }
+
+    private void initGame() {
+        timer = new Timer(timeDelay, new GameCycle());
+        timer.start();
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        doDrawing(g);
+    }
+
+    private class GameCycle implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            repaint();
+            checkCollision();
+        }
+    }
+
+    private void doDrawing(Graphics g) {
+        g.drawImage(bandy.bandicoot, bandy.getX(), bandy.getY(), this);
+        g.drawImage(button.exitbutton, button.getX(), button.getY(), this);
+        for(Bug r : bugs) {
+            g.drawImage(r.ladybug, r.getX(), r.getY(), this);
+            r.update();
+        }
+    }
+
+    private class TAdapter extends KeyAdapter {
+        public void keyPressed(KeyEvent e) {
+            bandy.checkForMotion(e);
+            button.checkForClick(e);
+        }
+    }
+
+    public void checkCollision(){
+        Rectangle bandBound = bandy.getBounds();
+        for( Bug r : bugs){
+            Rectangle bugBound = r.getBounds();
+            if(bandBound.intersects(bugBound)){
+                //remove from arraylist
+                score++;
+                //System.out.println(score);
+            }
+        }
+    }
+
+
+
+}
+
---
+++ Bug.java
@@ -0,0 +1,50 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Bug {
+    private int width;
+    private int height;
+    //initial x & y position
+    private int x = (int)(1100*Math.random() );
+    private int y = (int)(-10000*Math.random() - 100);
+    public Image ladybug;
+
+
+    public Bug(){
+        loadPicture();
+        update();
+    }
+
+    private void loadPicture(){
+        ImageIcon lady = new ImageIcon("src/imageFiles/ladybug.png");
+        ladybug = lady.getImage();
+        width = ladybug.getWidth(null);
+        height = ladybug.getHeight(null);
+    }
+
+    public void update(){
+        //if bug is still above ground, fall down 3 pixels
+        if(y <= 600){
+            y += 10;
+        }
+        //no else needed, if y == 0 remove bug object from arraylist in board class.
+    }
+
+    public Image getImage(){
+        return ladybug;
+    }
+
+    public int getX(){
+        return x ;
+    }
+
+    public int getY(){
+        return y;
+    }
+
+    public Rectangle getBounds(){
+        return new Rectangle(x, y, width, height);
+    }
+
+
+}
